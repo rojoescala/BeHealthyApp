9fbde177df4c7e4d5f0bb168017db6c6bd35ef78
Zetallay <rober898248_@hotmail.com>
2020-03-28 13:14:21 -0700

    tituloMenu

--- app/src/main/java/escalante/roberto/behealthy/menu.kt
+++ app/src/main/java/escalante/roberto/behealthy/menu.kt
@@ -1,12 +1,29 @@
 package escalante.roberto.behealthy
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import kotlinx.android.synthetic.main.activity_menu.*
+
+
 class menu : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        salud.text = getString(R.string.salud)
+        salud.typeface = Typeface.createFromAsset(assets, "fonts/modern.otf")
+
+
+
+
+
+
     }
+
+
+
 }
