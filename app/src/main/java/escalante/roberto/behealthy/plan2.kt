bb83841d1b39efe773a2ccc428a5a055bc8adc1a
Christian Galarza <christianglrza99@gmail.com>
2020-04-19 15:16:02 -0700

    pantalla plan

---
+++ app/src/main/java/escalante/roberto/behealthy/plan2.kt
@@ -0,0 +1,12 @@
+package escalante.roberto.behealthy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class plan2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plan2)
+    }
+}
