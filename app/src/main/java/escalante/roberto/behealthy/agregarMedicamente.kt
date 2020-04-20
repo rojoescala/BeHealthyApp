23a182d77398b9d5e301608425e03fc053a083fa
Zetallay <rober898248_@hotmail.com>
2020-04-20 12:22:53 -0600

    Pantalla agregar medicamentos

---
+++ app/src/main/java/escalante/roberto/behealthy/agregarMedicamente.kt
@@ -0,0 +1,12 @@
+package escalante.roberto.behealthy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class agregarMedicamente : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_medicamente)
+    }
+}
