960c9e4f1fa60ca80961061bcb0904c060d369e4
Zetallay <rober898248_@hotmail.com>
2020-04-23 10:23:04 -0600

    Intento de pasar pantallas

--- app/src/main/java/escalante/roberto/behealthy/menu.kt
+++ app/src/main/java/escalante/roberto/behealthy/menu.kt
@@ -1,12 +1,23 @@
 package escalante.roberto.behealthy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class menu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var intento: Intent = Intent(this,Medicamentos::class.java)
+
+        var boton: TextView = findViewById(R.id.btn_medicamentos) as TextView
+
+        boton.setOnClickListener{
+            startActivity(intento)
+        }
     }
 }
--- app/src/main/java/escalante/roberto/behealthy/sign_in.kt
+++ app/src/main/java/escalante/roberto/behealthy/sign_in.kt
@@ -1,12 +1,29 @@
 package escalante.roberto.behealthy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class sign_in : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        var intento: Intent = Intent(this,android.R.menu::class.java)
+
+        var boton:Button = findViewById(R.id.btn_signin) as Button
+
+        boton.setOnClickListener{
+            startActivity(intento)
+        }
+
+
+
+
+
+
     }
 }
