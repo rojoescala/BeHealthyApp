9e10cccc4234c9a6d640a6e4d48bdd2e9d318ae2
Christian Galarza <christianglrza99@gmail.com>
2020-04-28 14:28:30 -0700

    Commit atras adelante de plan y login con google

--- app/src/main/java/escalante/roberto/behealthy/plan.kt
+++ app/src/main/java/escalante/roberto/behealthy/plan.kt
@@ -11,11 +11,16 @@ class plan : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plan1)
         var intent = Intent(this, Menu::class.java)
+        var intent2 = Intent(this, plan2::class.java)
 
         var botonAtras: ImageView = findViewById(R.id.atras) as ImageView
+        var botonSiguiente: ImageView = findViewById(R.id.siguiente) as ImageView
 
         botonAtras.setOnClickListener{
             startActivity(intent)
+        }
+        botonSiguiente.setOnClickListener{
+            startActivity(intent2)
         }
     }
 }
