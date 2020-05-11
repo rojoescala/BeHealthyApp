aeaef76298e69b6c7528cbcbb898f337eda5c188
Christian Galarza <christianglrza99@gmail.com>
2020-05-11 16:48:33 -0700

    Notificaciones ¡No olvides tomar agua!

--- app/src/main/java/escalante/roberto/behealthy/Sign_in.kt
+++ app/src/main/java/escalante/roberto/behealthy/Sign_in.kt
@@ -1,7 +1,14 @@
 package escalante.roberto.behealthy
 
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.SystemClock
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 //import androidx.fragment.app.FragmentActivity
@@ -12,6 +19,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
+import escalante.roberto.behealthy.utilies.ReminderBroadcaster
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
 const val RC_SIGN_IN = 343
@@ -21,6 +29,8 @@ class Sign_in : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        createNotificationCannel()
 
         // Configure sign-in to request the user's ID, email address, and basic
 // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -75,7 +85,32 @@ class Sign_in : AppCompatActivity() {
     private fun updateUI(account: GoogleSignInAccount?) {
         if (account != null){
             val intent = Intent(this, Menu::class.java)
+            val intent2=Intent(this, ReminderBroadcaster::class.java)
+            var pendingIntent:PendingIntent= PendingIntent.getBroadcast(this,0,intent2,0)
+
+            var alarmManager: AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            var timeAtButtonClick= SystemClock.elapsedRealtime()
+            var hour=AlarmManager.INTERVAL_HOUR
+
+            alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,timeAtButtonClick+hour,hour,pendingIntent)
+
             startActivity(intent)
+        }
+    }
+
+
+
+    private fun createNotificationCannel(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            var name:CharSequence="TomaAguaChannel"
+            var description:String="Channel toma agua"
+
+            var importante:Int= NotificationManager.IMPORTANCE_DEFAULT
+            var channel: NotificationChannel =NotificationChannel("tomaAgua",name,importante)
+            channel.setDescription(description)
+
+            var noManagger:NotificationManager=getSystemService(NotificationManager::class.java)
+            noManagger.createNotificationChannel(channel)
         }
     }
 }
